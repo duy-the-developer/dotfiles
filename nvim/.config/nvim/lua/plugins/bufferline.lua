8f449c28c61b638a5b50321f8779d9eaba9a7d1e
Duy Nguyen <duythedeveloper@gmail.com>
2024-10-09 00:22:42 -0400

    more bufferline formatting logic to handle long ass file names

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,3 +1,36 @@
+local max_name_length = 50
+local function format_path(path)
+  -- Split the path into components
+  local components = {}
+  for component in string.gmatch(path, "[^/]+") do
+    table.insert(components, component)
+  end
+
+  local length = #components
+  local formatted = ""
+
+  if length > 3 then
+    -- If more than 2 parents, replace with "../"
+    formatted = "../"
+    -- Add the first letter of the last two directories
+    formatted = formatted .. components[length - 2]:sub(1, 2) .. "/" .. components[length - 1]:sub(1, 2) .. "/"
+  else
+    -- Add the first letter of each parent directory
+    for i = 1, length - 1 do
+      formatted = formatted .. components[i]:sub(1, 2) .. "/"
+    end
+  end
+
+  -- Append the full filename
+  formatted = formatted .. components[length]
+
+  if #formatted > max_name_length then
+    return "..." .. formatted:sub(-max_name_length + 3)
+  else
+    return formatted
+  end
+end
+
 return {
   "akinsho/bufferline.nvim",
   event = "VeryLazy",
@@ -15,6 +48,11 @@ return {
   opts = {
     options = {
       always_show_bufferline = true,
+      name_formatter = function(buffer)
+        return format_path(buffer.path)
+      end,
+      max_name_length = max_name_length,
+      show_duplicate_prefix = false,
     },
   },
 }
